41ddb56ad7d9b43773fe4947abc484ba3bfa9633
ubuntu <ubuntu@ip-172-26-11-99>
2019-01-19 14:07:31 +0000

    Ticket #97: Updated examples.

--- LSB.py
+++ LSB.py
@@ -181,3 +181,4 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
   			  	
   		    
   		    
+  		   	
--- tools.py
+++ tools.py
@@ -145,3 +145,4 @@ def base642binary(b64_fname):
   			  	
   		    
   		    
+  		   	
