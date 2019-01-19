aa9c7ddfea26fb961c1b1858cf0ee340d117bdbf
ubuntu <ubuntu@ip-172-26-11-99>
2019-01-19 14:11:18 +0000

    Ticket #42: Closes #2.

--- LSB.py
+++ LSB.py
@@ -251,3 +251,4 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
   			  	
   		    
   		    
+  		   	
--- tools.py
+++ tools.py
@@ -215,3 +215,4 @@ def base642binary(b64_fname):
   			  	
   		    
   		    
+  		   	
