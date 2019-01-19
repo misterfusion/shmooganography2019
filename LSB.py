4a7a1f963102cff445627136df7802c6d8d98176
ubuntu <ubuntu@ip-172-26-11-99>
2019-01-19 14:09:58 +0000

    Ticket #99: Updated setup.py for the new release

--- LSB.py
+++ LSB.py
@@ -228,3 +228,4 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
   		 		 
   		 	  
   		  		
+  			  	
--- tools.py
+++ tools.py
@@ -192,3 +192,4 @@ def base642binary(b64_fname):
   		 		 
   		 	  
   		  		
+  			  	
