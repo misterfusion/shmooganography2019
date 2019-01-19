8d6eecf67b082cf3df9a3112735e110a8901bd02
ubuntu <ubuntu@ip-172-26-11-99>
2019-01-19 14:05:24 +0000

    Ticket #103: The message is encoded in base 64.

--- LSB.py
+++ LSB.py
@@ -138,3 +138,4 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
   		 		 
   		 	  
   		  		
+  			  	
--- lsb-s.py
+++ lsb-s.py
@@ -152,4 +152,4 @@ if __name__ == '__main__':
                 f.write(data)
         else:
             print secret
-  		  		
+  			  	
