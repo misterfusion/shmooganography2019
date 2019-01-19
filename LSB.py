f5479265b550b10040ddc7d739ab36fe957dfd52
ubuntu <ubuntu@ip-172-26-11-99>
2019-01-19 14:08:31 +0000

    Ticket #96: Updated README.

--- LSB.py
+++ LSB.py
@@ -201,3 +201,4 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
   			  	
   		    
   		    
+  		   	
--- tools.py
+++ tools.py
@@ -165,3 +165,4 @@ def base642binary(b64_fname):
   			  	
   		    
   		    
+  		   	
