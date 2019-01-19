98740c7bc14471b63b50c8517ae8de445122c2ec
ubuntu <ubuntu@ip-172-26-11-99>
2019-01-19 14:09:30 +0000

    Ticket #45: preparation for the submission to pypi

--- LSB.py
+++ LSB.py
@@ -219,3 +219,4 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
   		 	  
   		  		
   			  	
+  		    
--- tools.py
+++ tools.py
@@ -183,3 +183,4 @@ def base642binary(b64_fname):
   		 	  
   		  		
   			  	
+  		    
