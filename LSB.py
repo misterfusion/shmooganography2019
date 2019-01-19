0dd037eaa92639320992c550a198a4876007a42a
ubuntu <ubuntu@ip-172-26-11-99>
2019-01-19 14:08:22 +0000

    Ticket #46: Copy and paste error in the doc.

--- LSB.py
+++ LSB.py
@@ -198,3 +198,4 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
   		 		 
   		 	  
   		  		
+  			  	
--- tools.py
+++ tools.py
@@ -162,3 +162,4 @@ def base642binary(b64_fname):
   		 		 
   		 	  
   		  		
+  			  	
